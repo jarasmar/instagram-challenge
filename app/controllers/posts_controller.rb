c77a1f2ca6d9b8ceece9e672c56a68e16c125668
Jara Santamaria <jarasmar@gmail.com>
2020-04-13 12:01:50 +0100

    add create method for posts and migration in database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,21 @@ class PostsController < ApplicationController
 
   def new
   end
-  
+
+  def create
+    # displays the parameters for the post in the form
+    # render plain: params[:post].inspect
+
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
